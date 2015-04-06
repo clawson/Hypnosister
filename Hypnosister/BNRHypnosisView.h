54650552bfb1a687e61f949241c8a997c84b9841
Dan Clawson <clawson@uen.org>
2015-04-05 19:10:14 -0600

    Add firstView example, and now the new files.

---
+++ Hypnosister/BNRHypnosisView.h
@@ -0,0 +1,14 @@
+//
+//  BNRHypnosisView.h
+//  Hypnosister
+//
+//  Created by Dan Clawson on 4/5/15.
+//  Copyright (c) 2015 Slacker Tools. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface BNRHypnosisView : UIView
+
+@end
---
+++ Hypnosister/BNRHypnosisView.m
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisView.m
+//  Hypnosister
+//
+//  Created by Dan Clawson on 4/5/15.
+//  Copyright (c) 2015 Slacker Tools. All rights reserved.
+//
+
+#import "BNRHypnosisView.h"
+
+@implementation BNRHypnosisView
+
+@end
