b48bc873a0a107ab7b4a2210c8f6c6d18c7d27f5
Dan Clawson <clawson@uen.org>
2015-04-05 18:28:38 -0600

    Initial commit

---
+++ Hypnosister/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  Hypnosister
+//
+//  Created by Dan Clawson on 4/5/15.
+//  Copyright (c) 2015 Slacker Tools. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
---
+++ Hypnosister/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Hypnosister
+//
+//  Created by Dan Clawson on 4/5/15.
+//  Copyright (c) 2015 Slacker Tools. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+
+@interface ViewController : UIViewController
+
+
+@end
